1988908c8111d80f7e91a3ce503056aa99b59257
Yuenmin <u2005375@siswa.um.edu.my>
2022-04-19 15:55:25 +0800

    add top nav bar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,57 +1,98 @@
 const template = document.createElement("template");
 template.innerHTML = `
-  <div class="sidebar">
-    <div class="header">
+  <div class="nav-wrapper">
+    <div class="navbar">
       <div class="logo">
-        <span>
-          Goal Progress
-          <br />
+        <h4>
+          Goal Progress <br />
           Tracker System
-        </span>
+        </h4>
       </div>
       <i class="bi-chevron-left nav-link" id="toggle-btn" tabindex="0"></i>
+      <i
+        class="bi bi-bell-fill nav-link"
+        tabindex="0"
+        data-bs-toggle="tooltip"
+        title="Notifications"
+      ></i>
+      <span class="translate-middle badge rounded-pill bg-danger">9+</span>
+      <div class="dropdown me-1">
+        <button
+          class="btn dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+        >
+          <img
+            src="https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU0NjQzOTk4OTQ4OTkyMzQy/ansel-elgort-poses-for-a-portrait-during-the-baby-driver-premiere-2017-sxsw-conference-and-festivals-on-march-11-2017-in-austin-texas-photo-by-matt-winkelmeyer_getty-imagesfor-sxsw-square.jpg"
+            class="avatar"
+          />
+        </button>
+        <ul class="dropdown-menu">
+          <li>
+            <a class="dropdown-item" href="#">
+              Profile
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">
+              Sign Out
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <hr/>
-    <nav class="nav nav-pills flex-column">
-      <a
-        class="nav-link"
-        href="index.html"
-        data-bs-toggle="tooltip"
-        title="Home"
-      >
-        <i class="bi-house-door-fill"></i>
-        <span>Home</span>
-      </a>
-      <a class="nav-link" href="#" data-bs-toggle="tooltip" title="Goals">
-        <i class="bi-flag-fill"></i>
-        <span>Goals</span>
-      </a>
-      <a class="nav-link" href="#" data-bs-toggle="tooltip" title="Reminders">
-        <i class="bi-alarm-fill"></i>
-        <span>Reminders</span>
-      </a>
-      <a class="nav-link" href="mentee.html" data-bs-toggle="tooltip" title="Mentees">
-        <i class="bi-people-fill"></i>
-        <span>Mentees</span>
-      </a>
-      <a class="nav-link" href="#" data-bs-toggle="tooltip" title="Setting">
-        <i class="bi-gear-fill"></i>
-        <span>Settings</span>
-      </a>
-      <a class="nav-link" href="profile.html" data-bs-toggle="tooltip" title="Profile">
-        <i class="bi-person-circle"></i>
-        <span>Profile</span>
-      </a>
-      <a
-        class="nav-link logout"
-        href="login.html"
-        data-bs-toggle="tooltip"
-        title="Logout"
-      >
-        <i class="bi-door-open-fill"></i>
-        <span>Logout</span>
-      </a>
-    </nav>
+    <div class="sidebar">
+      <nav class="nav nav-pills flex-column">
+        <a
+          class="nav-link"
+          href="index.html"
+          data-bs-toggle="tooltip"
+          title="Home"
+        >
+          <i class="bi-house-door-fill"></i>
+          <span>Home</span>
+        </a>
+        <a class="nav-link" href="#" data-bs-toggle="tooltip" title="Goals">
+          <i class="bi-flag-fill"></i>
+          <span>Goals</span>
+        </a>
+        <a class="nav-link" href="#" data-bs-toggle="tooltip" title="Reminders">
+          <i class="bi-alarm-fill"></i>
+          <span>Reminders</span>
+        </a>
+        <a
+          class="nav-link"
+          href="mentee.html"
+          data-bs-toggle="tooltip"
+          title="Mentees"
+        >
+          <i class="bi-people-fill"></i>
+          <span>Mentees</span>
+        </a>
+        <a class="nav-link" href="#" data-bs-toggle="tooltip" title="Setting">
+          <i class="bi-gear-fill"></i>
+          <span>Settings</span>
+        </a>
+        <a
+          class="nav-link"
+          href="profile.html"
+          data-bs-toggle="tooltip"
+          title="Profile"
+        >
+          <i class="bi-person-circle"></i>
+          <span>Profile</span>
+        </a>
+        <a
+          class="nav-link logout"
+          href="login.html"
+          data-bs-toggle="tooltip"
+          title="Logout"
+        >
+          <i class="bi-door-open-fill"></i>
+          <span>Logout</span>
+        </a>
+      </nav>
+    </div>
   </div>
 `;
 
@@ -63,17 +104,20 @@ class NavBar extends HTMLElement {
     document.getElementsByTagName("head")[0].appendChild(linkElem);
     this.appendChild(template.content.cloneNode(true));
     this.setActiveTab();
-    const tooltipList = this.getTooltip();
-    this.querySelector("#toggle-btn").addEventListener("click", () => {
+    this.initTooltip(".navbar i", "bottom");
+    const sidebarTooltip = this.initTooltip(".sidebar a", "right", true);
+    document.querySelector("#toggle-btn").addEventListener("click", () => {
       document.querySelector(".content-wrapper").classList.toggle("min");
-      const isClosed = this.querySelector(".sidebar").classList.toggle("min");
-      const icon = document.querySelector(".sidebar .header i");
+      const isClosed = document
+        .querySelector(".sidebar")
+        .classList.toggle("min");
+      const icon = document.querySelector("#toggle-btn");
       if (isClosed) {
         icon.classList.replace("bi-chevron-left", "bi-chevron-right");
       } else {
         icon.classList.replace("bi-chevron-right", "bi-chevron-left");
       }
-      tooltipList.forEach((element) => {
+      sidebarTooltip.forEach((element) => {
         if (isClosed) {
           element.enable();
         } else {
@@ -83,15 +127,16 @@ class NavBar extends HTMLElement {
     });
   }
 
-  getTooltip() {
+  initTooltip(path, placement, disable = false) {
     return [].slice
-      .call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+      .call(document.querySelectorAll(`${path}[data-bs-toggle="tooltip"]`))
       .map((element) => {
         const tooltip = new bootstrap.Tooltip(element, {
-          placement: "right",
-          delay: { show: 300, hide: 0 },
+          placement: placement,
+          trigger: "hover",
+          delay: { show: 500, hide: 0 },
         });
-        tooltip.disable();
+        if (disable) tooltip.disable();
         return tooltip;
       });
   }
