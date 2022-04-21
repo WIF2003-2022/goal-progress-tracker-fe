e9c6b13ec7179e47bbba7165ee92f2d8e7e828d3
JBLew <autumnlewjb@gmail.com>
2022-04-21 23:43:36 +0800

    reformat all files with prettier and add format on save config

--- public/js/delete account.js
+++ public/js/delete account.js
@@ -1,28 +1,36 @@
 // Call function when delete account button is clicked
-document.getElementById("deletebutton").onclick = function(){show_dialog()};
+document.getElementById("deletebutton").onclick = function () {
+  show_dialog();
+};
 var overlayme = document.getElementById("dialog-container");
 
 /* A function to show the dialog window */
-function show_dialog(){
-    overlayme.style.display = "block";
+function show_dialog() {
+  overlayme.style.display = "block";
 }
 
 // If ok btn is clicked , the function ok() is executed
-document.getElementById("confirm").onclick = function(){confirm()};
+document.getElementById("confirm").onclick = function () {
+  confirm();
+};
 var overlayagain = document.getElementById("message");
-function confirm(){
-    /* code executed if confirm is clicked */  
-    overlayagain.style.display = "block";
+function confirm() {
+  /* code executed if confirm is clicked */
+  overlayagain.style.display = "block";
 }
 
-document.getElementById("close").onclick = function(){close()};
-function close(){
-    location.replace("register.html")
+document.getElementById("close").onclick = function () {
+  close();
+};
+function close() {
+  location.replace("register.html");
 }
 
 // If cancel btn is clicked , the function cancel() is executed
-document.getElementById("cancel").onclick = function(){cancel()};
+document.getElementById("cancel").onclick = function () {
+  cancel();
+};
 function cancel() {
- /* code executed if cancel is clicked */  
-    overlayme.style.display = "none";
+  /* code executed if cancel is clicked */
+  overlayme.style.display = "none";
 }
