bb157ef66cc2cec43890e3a5e62ce6ea306530f4
Yuenmin <u2005375@siswa.um.edu.my>
2022-04-21 19:13:14 +0800

    complete homepage structure

--- public/js/home.js
+++ public/js/home.js
@@ -3,6 +3,8 @@ const authorNode = document.querySelector(".author");
 const refreshBtn = document.querySelector(".refresh");
 function generateQuote() {
   const temp = refreshBtn.innerHTML;
+  quoteNode.style.visibility = "hidden";
+  authorNode.style.visibility = "hidden";
   refreshBtn.innerHTML = `<span class="spinner-border spinner-border-sm"></span>`;
   refreshBtn.disabled = true;
   fetch(
@@ -16,16 +18,22 @@ function generateQuote() {
     .finally(() => {
       refreshBtn.innerHTML = temp;
       refreshBtn.disabled = false;
+      quoteNode.style.visibility = "visible";
+      authorNode.style.visibility = "visible";
     });
 }
 document.addEventListener("DOMContentLoaded", () => {
   generateQuote();
+  setProgress();
   var calendarEl = document.getElementById("calendar");
   var calendar = new FullCalendar.Calendar(calendarEl, {
     themeSystem: "bootstrap5",
     headerToolbar: {
-      left: "prev,next today",
+      left: "prev",
       center: "title",
+      right: "next",
+    },
+    footerToolbar: {
       right: "dayGridMonth,timeGridDay,listWeek",
     },
     initialView: "listWeek",
@@ -34,3 +42,24 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 refreshBtn.addEventListener("click", () => generateQuote());
+
+function setProgress() {
+  const arr = [];
+  const spinners = document.querySelectorAll(".spinner");
+  for (let i = 0; i < spinners.length; i++) {
+    arr.push(Math.floor(Math.random() * 100));
+  }
+  spinners.forEach((e, index) => {
+    progress = arr[index];
+    e.style.background =
+      "conic-gradient(rgb(3, 133, 255) " +
+      progress +
+      "%,rgb(242, 242, 242) " +
+      progress +
+      "%)";
+  });
+  document.querySelectorAll(".middle-circle").forEach((e, index) => {
+    progress = arr[index];
+    e.innerHTML = progress.toString() + "%";
+  });
+}
