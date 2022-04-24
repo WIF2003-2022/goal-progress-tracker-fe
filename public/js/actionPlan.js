bab090bdf76321e7b89f4c35f6ead43b00c9abeb
J Sen Ooi <u2005254@siswa.um.edu.my>
2022-04-24 17:05:00 +0800

    Action Plan + Activity design enhancement

--- public/js/actionPlan.js
+++ public/js/actionPlan.js
@@ -1,100 +1,18 @@
-var img_ap =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRINomLSaFLHVYYfShk5a8DZ8SkubojQhUeLQ&usqp=CAU";
-AP = [
-  { title: "Action Plan 1", due: "DD/MM/YYY", image: img_ap },
-  { title: "Action Plan 2", due: "DD/MM/YYY", image: img_ap },
-  { title: "Action Plan 3", due: "DD/MM/YYY", image: img_ap },
-  { title: "Action Plan 4", due: "DD/MM/YYY", image: img_ap },
-  { title: "Action Plan 5", due: "DD/MM/YYY", image: img_ap },
-  { title: "Action Plan 6", due: "DD/MM/YYY", image: img_ap },
-  { title: "Action Plan 7", due: "DD/MM/YYY", image: img_ap },
-  { title: "Action Plan 8", due: "DD/MM/YYY", image: img_ap },
-  { title: "Action Plan 9", due: "DD/MM/YYY", image: img_ap },
-  { title: "Action Plan 10", due: "DD/MM/YYY", image: img_ap },
-];
-
-document.write("<ul>");
-document.write("<div class='row continueAdd'>");
-for (i = 0; i < 4; i++) {
-  document.write("<li class='card col-3 m-3 shadow' style='width: 22rem'>");
-  document.write("<div class='row'>");
-  document.write("<div class='col-9'></div>");
-  document.write(
-    "<div class='col-1'><a href='action-main-edit.html' style='text-decoration: none'><button onclick='myFunction(event)' class='changing' style='border: none; background: none;'><i class='bi-pencil' style='font-size: 1.5rem;'></i></button></a></div>"
-  );
-  document.write(
-    "<div class='col-1'><button class='close' style='border: none; background: none;'><i class='bi-trash-fill' style='font-size: 1.5rem;'></i></button></div>"
-  );
-  document.write("</div>");
-  if (AP[i].image.length > 0) {
-    document.write(
-      "<img class='card-img-top' src='" +
-        AP[i].image +
-        "' alt='Card image cap'></img>"
-    );
-  }
-  document.write("<div class='card-body'>");
-  document.write(
-    "<a href='activity.html' style='text-decoration: none;' class='card-text titleText'>" +
-      AP[i].title +
-      "</a>"
-  );
-  document.write("<p class='card-text dueText'>Due " + AP[i].due + "</p>");
-  document.write("</div>");
-  document.write("</li>");
+//delete funtion
+var elem = document.querySelectorAll(".deleteAP");
+for (i = 0; i < elem.length; i++) {
+  elem[i].addEventListener("click", function () {
+    if (confirm("Are you sure you want to delete this action plan?")) {
+      this.closest("li").remove();
+    }
+  });
 }
-document.write("</div>");
-document.write("</ul>");
 
+//JQuery
 /*
-function addHTML(newTitle, newDue, img = img_ap) {
-  return (
-    `
-  <li class="card col-3 m-3 shadow" style="width: 22rem">
-    <div class="row">
-      <div class="col-9"></div>
-      <div class="col-1"><button onclick="myFunction(event)" class="changing" style="border: none; background: none;"><i class="bi-pencil" style="font-size: 1.5rem;"></i></button></div>
-      <div class="col-1"><button class="close" style="border: none; background: none;"><i class="bi-trash-fill" style="font-size: 1.5rem;"></i></button></div>
-    </div>
-    <img class="card-img-top" src="` +
-    img +
-    `"alt="Card image cap"></img>
-    <div class="card-body">
-      <a href='activity.html' style='text-decoration: none;' class="card-text titleText">` +
-    newTitle +
-    `</a>
-      <p class="card-text dueText">Due ` +
-    newDue +
-    `</p>
-    </div>
-`
-  );
-}
-*/
-
-/*
-// add function
-$(".add").on("click", function () {
-  $(".continueAdd").append(addHTML("new Action Plan", "new DD/MM/YYYY"));
-});
-
-// edit function (under construction -- will change all text for the same tag)
-
-var change = document.querySelectorAll(".changing");
-var titleTexts = document.querySelectorAll(".titleText");
-var dueTexts = document.querySelectorAll(".dueText");
-console.log(change);
-function myFunction(event) {
-  alert(event.target.nodeValue);
-}
-
-
-$(".changing").click(function () {
-  $(".titleText").text("Hello world!");
-});
-*/
-
-// delete function (cannot delete newly added cards)
 $(".close").on("click", function () {
-  $(this).closest("li").remove();
+  if (confirm("Are you sure you want to delete this action plan?")) {
+    $(this).closest("li").remove();
+  }
 });
+*/
