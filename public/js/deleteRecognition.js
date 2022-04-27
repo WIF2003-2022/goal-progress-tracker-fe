aac50e578a25afb02f98c4890f98557ef699eebd
zhihui218 <u2005282@siswa.um.edu.my>
2022-04-28 00:01:03 +0800

    edited close button

--- public/js/deleteRecognition.js
+++ public/js/deleteRecognition.js
@@ -1,10 +1,7 @@
 var deleteTag = document.getElementsByClassName("delete-btn");
-var targetContent = document.getElementsByClassName("content");
 var i;
-function myfunction(){
-    for(i=0; i<deleteTag.length; i++){
-        deleteTag[i].addEventListener("click", function(){
-            this.parentElement.style.display = "none";
-        });
-    }
+for(i=0; i<deleteTag.length; i++){
+    deleteTag[i].addEventListener("click", function(){
+        this.parentElement.style.display = "none";
+    });
 }
