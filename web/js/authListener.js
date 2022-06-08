5377724b6cd1f7625cdcde0edd49deda6f8c3c48
JBLew <autumnlewjb@gmail.com>
2022-06-08 12:27:16 +0800

    Change project structure

--- web/js/authListener.js
+++ web/js/authListener.js
@@ -2,6 +2,6 @@ window.addEventListener("load", (e) => {
   const val = sessionStorage.getItem("auth");
   console.log(val !== "true");
   if (val !== "true") {
-    window.location.href = "login.html";
+    window.location.href = "../views/login.html";
   }
 });
