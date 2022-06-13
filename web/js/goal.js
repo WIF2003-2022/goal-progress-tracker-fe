b38d4276adeb350cb11dd8025d23012fe2ad7981
thaiming0123 <tanthaiming@gmail.com>
2022-06-13 12:14:31 +0800

    Add filter function in goal page

--- web/js/goal.js
+++ web/js/goal.js
@@ -1,4 +1,57 @@
+filterFolder("all"); // defaultly set it as displaying all goals
+function filterFolder(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") {
+    c = "";
+  }
+
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) {
+      addClass(x[i], "show");
+    }
+  }
+}
+
+// Show filtered elements
+function addClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add current class to the current control button (highlight it)
+var btns = document.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("btn current");
+    current[0].className = current[0].className.replace(" current", "");
+    this.className += " current";
+  });
+}
+
 //delete funtion
+/*
 var elem = document.querySelectorAll(".deleteGoal");
 var key = document.querySelector(".deleteButton");
 for (i = 0; i < elem.length; i++) {
@@ -10,6 +63,8 @@ for (i = 0; i < elem.length; i++) {
     });
   });
 }
+
+*/
 
 //JQuery
 /*
