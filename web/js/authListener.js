96523a6298931e266b981a40c5957ff918238afe
JBLew <autumnlewjb@gmail.com>
2022-06-14 13:03:50 +0800

    Avoid redirection in javascript

--- web/js/authListener.js
+++ web/js/authListener.js
@@ -1,7 +1,4 @@
 window.addEventListener("load", (e) => {
   const val = sessionStorage.getItem("auth");
   console.log(val !== "true");
-  // if (val !== "true") {
-  //   window.location.href = "login.html";
-  // }
 });
