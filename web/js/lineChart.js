0e900169f998555f167dccac2fd0f8d66a44e76d
jylim1001 <ljy_2000@hotmail.com>
2022-06-16 10:57:53 +0800

    graph visualisation update

--- web/js/lineChart.js
+++ web/js/lineChart.js
@@ -1,59 +1,4 @@
-var ctx = document.getElementById("polarChart").getContext("2d");
 var line = document.getElementById("lineChart").getContext("2d");
-var myChart = new Chart(ctx, {
-  type: "polarArea",
-  data: {
-    labels: ["Loss 3kg in 3 months                           ", "Read 2 books in 5 weeks                     ", "Wake up at 7am for 21 days                 ", "Manage expenses within RM1K in April", "Score 4.0 in this semester                     ", "Learn video-editing skill in 2 months      "],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [23, 50, 100, 81, 20, 60],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    responsive: true,
-    plugins: {
-      labels: {
-        render: (args) => {
-          return args.value + "%";
-        },
-      },
-      title: {
-        display: true,
-        text: "Goals Percentage",
-        font: {
-          size: 20,
-        },
-        padding: {
-          top: 10,
-          bottom: 30,
-        },
-      },
-      legend: {
-        position: "bottom",
-      },
-    },
-  },
-});
-
 var lineChart = new Chart(line, {
   type: "line",
   data: {
@@ -171,3 +116,4 @@ $.ajax({
   .always(function (xhr, status) {
     console.log("The request is complete!");
   });
+
---
+++ web/js/polarChart.js
@@ -0,0 +1,85 @@
+var ctx = document.getElementById("polarChart").getContext("2d");
+let goalName, goalPercentage;
+var myChart = new Chart(ctx, {
+  type: "polarArea",
+  data: {
+    labels: goalName,
+    datasets: [
+      {
+        data: goalPercentage,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      labels: {
+        render: (args) => {
+          return args.value + "%";
+        },
+      },
+      title: {
+        display: true,
+        text: "Goals Percentage",
+        font: {
+          size: 20,
+        },
+        padding: {
+          top: 10,
+          bottom: 30,
+        },
+      },
+      legend: {
+        position: "bottom",
+      },
+    },
+  },
+});
+
+$(document).ready(() => {
+  $.ajax({
+    url: "./src/getGoals.php",
+    type: "GET",
+    dataType: "json",
+  })
+    .done((datapoints) => {
+      // const datapoints = JSON.parse(this.responseText);
+      goalName = datapoints.map(function (index) {
+        return index.goal_title;
+      });
+      goalPercentage = datapoints.map(function (index) {
+        return index.goal_progress;
+      });
+      console.log(goalName);
+      console.log(goalPercentage);
+      myChart.data.labels = goalName;
+      myChart.data.datasets[0].data = goalPercentage;
+      myChart.update();
+    })
+    .fail((xhr, status, errorThrown) => {
+      alert("Sorry, there was a problem!");
+      console.log("Error: " + errorThrown);
+      console.log("Status: " + status);
+      console.dir(xhr);
+    })
+    .always(function (xhr, status) {
+      console.log("The request is complete!");
+    });
+});
