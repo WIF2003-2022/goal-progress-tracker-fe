4ee38e9e92475c8300c52da33f24ce94a198cfcb
JBLew <autumnlewjb@gmail.com>
2022-06-18 10:46:15 +0800

    Handle errors in reset password and email verification flow

--- web/src/handleForgotPassword.php
+++ web/src/handleForgotPassword.php
@@ -1,21 +1,33 @@
 <?php
 
-echo isset($_GET['token']);
 require_once @realpath(dirname(__FILE__) . '/services/decodeJWT.php');
 require_once @realpath(dirname(__FILE__) . '/services/updateUser.php');
+require_once @realpath(dirname(__FILE__) . "/services/checkIsSetAndNotEmpty.php");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['password']) && isset($_GET['token'])) {
+  if (!checkIsSetAndNotEmpty($_POST, ['password']) && !checkIsSetAndNotEmpty($_GET, ['token'])) {
+    header("Location: ../email-verification-failed.html");
+  }
   $jwt = $_GET['token'];
   $password = $_POST['password'];
 
-  $payload = decodeJWT($jwt);
-  $jwtExpiry = $payload->exp;
-
-  if ($jwtExpiry - time() > 0) {
-    header("Location: " . "../reset-password-success.html");
-    echo "JWT still valid";
-    updateUserPassword($payload->preferred_username, $payload->email, $password, $jwt);
-  } else {
-    echo "JWT not valid";
+  try {
+    $payload = decodeJWT($jwt);
+    $jwtExpiry = $payload->exp;
+  
+    if ($jwtExpiry - time() > 0) {
+      if (updateUserPassword($payload->preferred_username, $payload->email, $password, $jwt)) { 
+        header("Location: " . "../reset-password-success.html");
+        exit();
+      }
+    } else {
+      error_log("JWT not valid");
+    }
+  } catch (Exception $e) {
+    http_response_code(500);
+    header("Location: " . "../reset-password-failed.html");
+    exit();
   }
 }
+http_response_code(500);
+header("Location: " . "../reset-password-failed.html");
--- web/src/handleRequestEmail.php
+++ web/src/handleRequestEmail.php
@@ -8,54 +8,59 @@ require_once @realpath(dirname(__FILE__) . "/services/sendEmail.php");
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['email'])) {
   $email = $_POST['email'];
 
-  $user = findUserByEmail($conn, $email);
+  try {
+    $user = findUserByEmail($conn, $email);
 
-  if (!$user) {
-    exit();
-  }
+    if (!$user) {
+      http_response_code(404);
+      die('You might not have an account with this email address. Create one now!');
+    }
 
-  $requestURI = $_SERVER['REQUEST_URI'];
-  $tokens = explode("/", $requestURI);
-  $tokens[count($tokens) - 2] = 'forgot-password.php';
-  unset($tokens[count($tokens) - 1]);
-  $resetPasswordURI = implode("/", $tokens);
-
-  $host = $_SERVER['HTTP_HOST'];
-  $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https://' : 'http://';
-
-  $resetPasswordURL = $protocol . $host . $resetPasswordURI;
-
-  $resetPasswordLink = generateAndSaveResetPasswordLink($user['user_id'], $user['name'], $user['email'], $resetPasswordURL);
-
-  $mailingEmail = $_ENV['MAILING_USERNAME'];
-  $mailingPassword = $_ENV['MAILING_PASSWORD'];
-
-  $mailer = mailerObjFactory(
-    $mailingEmail,
-    $mailingPassword,
-    $mailingEmail,
-    'Goal Progress Tracker Support',
-    $mailingEmail,
-    'Goal Progress Tracker Support',
-    $user['name'],
-    $user['email'],
-  );
-
-  $subject = "Reset your password";
-  $body = "You have requested to reset your password. Go to this link to proceed $resetPasswordLink";
-  $altBody = "Reset your password";
-
-  header("Location: ../login.php");
-
-  if(!$mailer(
-    $subject,
-    $body,
-    $altBody
-  ))
-  {
-      echo 'Mailer Error: ' . $mail->ErrorInfo;
-  } else {
-      echo 'Message sent!';
+    $requestURI = $_SERVER['REQUEST_URI'];
+    $tokens = explode("/", $requestURI);
+    $tokens[count($tokens) - 2] = 'forgot-password.php';
+    unset($tokens[count($tokens) - 1]);
+    $resetPasswordURI = implode("/", $tokens);
+  
+    $host = $_SERVER['HTTP_HOST'];
+    $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https://' : 'http://';
+  
+    $resetPasswordURL = $protocol . $host . $resetPasswordURI;
+  
+    $resetPasswordLink = generateAndSaveResetPasswordLink($user['user_id'], $user['name'], $user['email'], $resetPasswordURL);
+  
+    $mailingEmail = $_ENV['MAILING_USERNAME'];
+    $mailingPassword = $_ENV['MAILING_PASSWORD'];
+  
+    $mailer = mailerObjFactory(
+      $mailingEmail,
+      $mailingPassword,
+      $mailingEmail,
+      'Goal Progress Tracker Support',
+      $mailingEmail,
+      'Goal Progress Tracker Support',
+      $user['name'],
+      $user['email'],
+    );
+  
+    $subject = "Reset your password";
+    $body = "You have requested to reset your password. Go to this link to proceed $resetPasswordLink";
+    $altBody = "Reset your password";
+  
+    if(!$mailer(
+      $subject,
+      $body,
+      $altBody
+    ))
+    {
+      error_log('Mailer Error: ' . $mail->ErrorInfo);
+      http_response_code(500);
+      die("We are not able to send email to you for the moment.");
+    }
+    echo json_encode("");
+  } catch (Exception $e) {
+    http_response_code(500);
+    die("We are not able to reset your password for the moment. Please try again later.");
   }
 }
 
