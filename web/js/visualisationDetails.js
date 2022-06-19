d73d33d67fde59bcbaa0d5bf234c174e0d69793b
jylim1001 <ljy_2000@hotmail.com>
2022-06-19 20:34:33 +0800

    Co-authored-by: JBLew <autumnlewjb@users.noreply.github.com>

---
+++ web/js/visualisationDetails.js
@@ -0,0 +1,94 @@
+$(document).ready(() => {
+  const generateActivity = ({ a_title, a_progress = "0" }) =>
+    `<li>
+    ${a_title}
+    <div class="progress">
+      <div
+        class="progress-bar"
+        role="progressbar"
+        style="width: ${a_progress}%"
+        aria-valuenow="${a_progress}"
+        aria-valuemin="0"
+        aria-valuemax="100"
+      >
+        ${a_progress}%
+      </div>
+    </div>
+  </li>`;
+
+  const generateActionPlan = ({ ap_title }, activities) =>
+    `<li>
+      ${ap_title}
+      <ul>
+        ${activities.join("")}
+      </ul>
+    </li>`;
+
+  const generateGoal = (
+    { goal_id, goal_status, goal_title, goal_start_date, goal_due_date },
+    actionPlans
+  ) =>
+    `<div class="filterDiv ${goal_status.toLowerCase()}">
+    <div class="box" style="background-color: rgb(249, 252, 180)">
+      <span
+        style="
+          font-size: 100%;
+          color: rgb(255, 109, 4);
+          font-family: 'roboto';
+        "
+      >
+        <i class="bi bi-activity"></i>
+        ${goal_status.toUpperCase()}
+      </span>
+      <div>
+        <div class="goal" id="goal-title">${goal_title}</div>
+        <span
+          style="
+            font-size: 80%;
+            color: rgb(165, 160, 160);
+            font-family: 'verdana';
+          "
+        >
+          Date: ${goal_start_date} - ${goal_due_date}
+        </span>
+        <div class="goalName">
+          <ul>
+            ${actionPlans.join("")}
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>`;
+  $.ajax({
+    url: "./src/visualisationDetails.php",
+    type: "GET",
+    dataType: "json",
+  })
+    .done((goals) => {
+      const goalLists = Object.values(goals).map((goal) =>
+        generateGoal(
+          goal,
+          Object.values(goal["action_plans"]).map((actionPlan) =>
+            generateActionPlan(
+              actionPlan,
+              actionPlan["activities"].map((activity) =>
+                generateActivity(activity)
+              )
+            )
+          )
+        )
+      );
+      console.log({ goalLists });
+      $(".objects").html(goalLists.join(""));
+      filterObjects("all");
+    })
+    .fail((xhr, status, errorThrown) => {
+      alert("Sorry, there was a problem!");
+      console.log("Error: " + errorThrown);
+      console.log("Status: " + status);
+      console.dir(xhr);
+    })
+    .always(function (xhr, status) {
+      console.log("The request is complete!");
+    });
+});
--- web/src/visualisationDetails.php
+++ web/src/visualisationDetails.php
@@ -9,15 +9,43 @@ if (!$user) {
     exit();
 }
 
- //fetch info from db
-$goal= "SELECT * FROM goal WHERE mentee_id = $user->user_id";
-$actionplan = "SELECT * FROM `action plan` INNER JOIN goal ON `action plan`.goal_id = goal.goal_id";
-$queryResult = $conn -> query($goal);
-$queryResult1 = $conn -> query($actionplan);
-$result = $queryResult -> fetch_all(MYSQLI_ASSOC); 
-$result1 = $queryResult1 -> fetch_all(MYSQLI_ASSOC);
-echo json_encode($result);
-echo json_encode($result1);
+// select all goal by the user
+// for each goal, find its action plans
+// for each action plan, find its activities
+
+$actionplan = "SELECT goal.goal_id, ap.ap_id, activity.a_id, goal.goal_status, goal.goal_title, goal.goal_start_date, goal.goal_due_date, ap.ap_title, activity.a_title, activity.a_progress FROM `action plan` AS ap INNER JOIN goal ON ap.goal_id = goal.goal_id INNER JOIN activity ON ap.ap_id = activity.ap_id WHERE mentee_id = 100;";
+$queryResult = $conn -> query($actionplan);
+$result = $queryResult -> fetch_all(MYSQLI_ASSOC);
+
+$goals = array();
+foreach ($result as $goal) {
+    // var_dump($result);
+    // echo "<br>";
+    if (!isset($goals[$goal['goal_id']])) {
+        $goals[$goal['goal_id']] = array(
+            "goal_id" => $goal['goal_id'],
+            "goal_title" => $goal['goal_title'],
+            "goal_status" => $goal['goal_status'],
+            "goal_start_date" => $goal['goal_start_date'],
+            "goal_due_date" => $goal['goal_due_date'],
+            "action_plans" => array()
+        );
+    }
+    if (!isset($goals[$goal['goal_id']]['action_plans'][$goal['ap_id']])) {
+        $goals[$goal['goal_id']]['action_plans'] += array($goal['ap_id'] => array(
+            "ap_id" => $goal['ap_id'],
+            "ap_title" => $goal['ap_title'],
+            "activities" => array()
+        ));
+    }
+    $currActivity = array(
+        "a_id" => $goal['a_id'],
+        "a_title" => $goal['a_title'],
+        "a_progress" => $goal["a_progress"],
+    );
+    array_push($goals[$goal['goal_id']]['action_plans'][$goal['ap_id']]['activities'], $currActivity);
+}
+
+echo json_encode($goals);
 $queryResult -> free_result();
-$queryResult1 -> free_result();
 $conn -> close();
