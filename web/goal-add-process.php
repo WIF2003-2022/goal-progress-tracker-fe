a1397c529c8bec3388a9c38709e303b4f7de83f0
CXiann <tungchunxian@gmail.com>
2022-06-20 02:26:04 +0800

    recent - inserted sql to update recent table in goal

--- web/goal-add-process.php
+++ web/goal-add-process.php
@@ -1,7 +1,7 @@
 <?php 
 require_once @realpath(dirname(__FILE__) . "/config/databaseConn.php");
 $title = $_POST['title'];
-$target = $POST['target'];
+$target = $_POST['target'];
 $startDate = $_POST['startDate'];
 $endDate = $_POST['endDate'];
 $tracking = $_POST['tracking'];
@@ -22,8 +22,14 @@ else{
   $addsql = "INSERT INTO `goal` (`mentee_id`,`mentor_id`,`goal_title`,`goal_target`,`goal_description`,`goal_category`,`goal_status`,`goal_progress`,`tracking_method`,`goal_start_date`,`goal_due_date`) 
                     values ('$mentee', '$mentor', '$title','$target', '$description','$category', '$status', '$progress', '$tracking', '$startDate', '$endDate' )";
 }
+
 $addquery= mysqli_query($conn,$addsql);
+
 $lastId = mysqli_insert_id($conn);
+$recentsql = "INSERT INTO `recent` (`r_type`, `user_id`, `updated_id`, `action`) 
+                    values('goal', '$mentee', '$lastId', 'add')";
+                    
+mysqli_query($conn,$recentsql);
 
 if($addquery ==true){
   $data = array(
