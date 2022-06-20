96d6e1cd065fb07f0f5afcfb798723bb4cd78ea5
CXiann <tungchunxian@gmail.com>
2022-06-21 02:20:01 +0800

    date_diff

--- web/activity-delete.php
+++ web/activity-delete.php
@@ -1,5 +1,8 @@
 <?php
   include_once @realpath(dirname(__FILE__) . "/../web/config/databaseConn.php");
+  require_once @realpath(dirname(__FILE__) . "/src/services/checkAuthenticated.php");
+
+  $userID = json_decode($_SESSION['auth'],true)['user_id'];
   $a_id = $_GET["a_id"];
   $sql = "SELECT * FROM activity WHERE a_id = $a_id";
   $result = $conn -> query($sql);
@@ -7,6 +10,8 @@
   $ap_id = $row["ap_id"];
   $sql = "DELETE FROM activity WHERE a_id = $a_id";
   $result = $conn -> query($sql);
+  $deleteRecentsql = "DELETE FROM recent WHERE updated_id = $a_id AND user_id = $userID AND r_type = "."'activity'";
+  $result = $conn -> query($deleteRecentsql);
   $sql = "SELECT * FROM `action plan` WHERE ap_id = $ap_id";
   $result = $conn -> query($sql);
   $row = $result -> fetch_assoc();
--- web/goal-delete.php
+++ web/goal-delete.php
@@ -1,9 +1,13 @@
 <?php 
 require_once @realpath(dirname(__FILE__) . "/config/databaseConn.php");
+require_once @realpath(dirname(__FILE__) . "/src/services/checkAuthenticated.php");
 
+$userID = json_decode($_SESSION['auth'],true)['user_id'];
 $goal = $_POST["id"];
 $deletesql = "DELETE FROM goal WHERE `goal`. `goal_id` =$goal";
+$deleteRecentsql = "DELETE FROM recent WHERE updated_id = $goal AND user_id = $userID AND r_type = "."'goal'";
 $delQuery =mysqli_query($conn,$deletesql);
+$delQuery =mysqli_query($conn,$deleteRecentsql);
 if($delQuery==true)
 {
 	 $data = array(
